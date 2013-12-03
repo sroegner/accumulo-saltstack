3f357908418b86143bef6b7ba32c90b022f693d3
Steffen Roegner <steffen.roegner@gmail.com>
2013-12-03 16:47:31 -0500

    support ubuntu in jenkins ci, updated ci configuration

--- test/jenkins/destroy_map.sh
+++
@@ -1,7 +0,0 @@
-N=${BUILD_EXECUTOR:-1}
-cd $(dirname $0)
-WS=$(pwd)
-SALT_CLOUD=${SALT_CLOUD_PATH:-/home/jenkins/virtual/bin/salt-cloud}
-SALT_CLOUD_OPTS="--profiles=${WS}/jenkins.profiles --map=${WS}/jenkins.map"
-
-sudo ${SALT_CLOUD} ${SALT_CLOUD_OPTS} -d -y
