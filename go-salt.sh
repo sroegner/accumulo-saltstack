d0175110061aec30e3c45993b4fbd62cbe36a41d
Steffen Roegner <steffen.roegner@gmail.com>
2015-02-14 10:41:56 -0500

    improve execution feedback

--- go-salt.sh
+++ go-salt.sh
@@ -1,2 +1,4 @@
+echo "Calling the provisioning script inside vagrant now - this will take a while and there will be no output for some time"
+
 vagrant ssh namenode -c /vagrant/vagrant-bootstrap/orchestrate.sh
 
